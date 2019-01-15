e7b5a7e7992f6a3367f8433d71c9f85c69d87e1a
glezluis <luismg@tosha.attlocal.net>
2019-01-15 14:18:20 -0800

    fixed model

--- specials/models.py
+++ specials/models.py
@@ -9,6 +9,9 @@ class Post(models.Model):
 	date_posted = models.DateField(default=timezone.now)
 	image = models.ImageField(upload_to='images', blank=True, null=True)
 	#author = models.ForeignKey(users.User, on_delete=models.CASCADE)
+	def image_url(self):
+		if self.image and hasattr(self.image, 'url'):
+			return self.image.url
 
 	def __str__(self):
 		return self.title
