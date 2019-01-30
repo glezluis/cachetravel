c97ca7795dca1cf704c133b61f7a8d1e9b59f79d
glezluis <luismg@csu.fullerton.edu>
2019-01-30 00:44:37 +0000

    Change to home main and models

--- specials/models.py
+++ specials/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
 	content = models.TextField()
 	date_posted = models.DateField(default=timezone.now)
 	image = models.ImageField(upload_to='images', blank=True, null=True)
-	#author = models.ForeignKey(users.User, on_delete=models.CASCADE)
+	author = models.ForeignKey(users.User, on_delete=models.CASCADE)
 	def image_url(self):
 		if self.image and hasattr(self.image, 'url'):
 			return self.image.url
